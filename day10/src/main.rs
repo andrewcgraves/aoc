a7d4c2c49122962217d291da0243701f7018fa7e
andrew (m) <andrewcgraves@gmail.com>
2021-12-10 09:57:07 -0800

    initial

---
+++ day10/src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading this file");
+
+    println!("\nOutput: {}", contents);
+
+}
