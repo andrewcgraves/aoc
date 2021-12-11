3cb9c3a082dbf7b6406ddbb8bb66f5f9d010d583
andrew (m) <andrewcgraves@gmail.com>
2021-12-10 21:24:33 -0800

    step 1 day 10, I forgot rust was optimised

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,6 +5,45 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let contents = fs::read_to_string(&args[1]).expect("Something went wrong reading this file");
 
-    println!("\nOutput: {}", contents);
+    println!("Finished with an error score of {}", task_1(contents));
+}
 
+fn task_1(input: String) -> i32 {
+    let mut stack: Vec<char> = Vec::new();
+    let mut expected_value: Option<char>;
+    let mut line_score = 0;
+
+    for line in input.lines() {
+        for c in line.chars() {
+            if c == '[' || c == '<' || c == '(' || c == '{' {
+                stack.push(c);
+            }
+            else {
+                expected_value = stack.pop();
+    
+                if expected_value != closer_to_opener(c)
+                {
+                    line_score += match c {
+                        ')' => 3,
+                        ']' => 57,
+                        '}' => 1197,
+                        '>' => 25137,
+                        _ => 0
+                    }
+                }
+            }
+        }
+    }
+
+    return line_score;
+}
+
+fn closer_to_opener(c: char) -> Option<char> {
+    match c {
+        ')' => Some('('),
+        ']' => Some('['),
+        '>' => Some('<'),
+        '}' => Some('{'),
+        _ => None,
+    }
 }
